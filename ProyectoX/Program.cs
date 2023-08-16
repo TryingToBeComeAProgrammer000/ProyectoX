54a4c3f3e651179126bb53f3836e25e8e72796ad
Kendallvj <keneth11about@gmail.com>
2023-08-15 21:50:56 -0600

    Kendall

--- ProyectoX/Program.cs
+++ ProyectoX/Program.cs
@@ -6,7 +6,7 @@ namespace ProyectoX
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Definitivo");
+            Console.WriteLine("Definitivo");
         }
     }
 }
